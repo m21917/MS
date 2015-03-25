7f73f799bd689809f0e861e2517cb5c4ded5b85d
m21917 <m21917@gmail.com>
2015-03-25 10:18:50 +0800

    加入檔案
    加入檔案

---
+++ index.php
@@ -0,0 +1,246 @@
+<?php
+	require_once("initPage.php");
+
+	init_header("明日創作主題文章設計競賽");
+?>
+	
+	<script type="text/javascript" src="js/index_js.js"></script>
+
+	<!--========================header頂===============================-->
+	<div class="row" style="background-image:url(image/book-background.jpg);-moz-background-size: cover;background-size: cover;">
+	<div class="col-lg-12 col-sm-12 col-xs-12" id="header">
+	
+	<div style="width:800px;float:left;margin-left:20px;margin-top:10px">
+	<span style="font-size:30px">桃園市104年度</span><br>
+	<span style="margin-top:5px;font-size:45px"><strong>明日創作主題文章設計競賽</strong></span>
+	</div>
+	<div style="float:right">
+	<img src="image/School of Tomorrow.png" style="float:right;margin-top:38px;">
+	</div>
+	<div id="navbar-meun">
+	<!-- <ul class="nav nav-pills">
+	  <li role="presentation" class="active btn-lg"><a href="#" value="section1">活動宗旨</a></li>
+	  <li role="presentation" class="btn-lg"><a href="#" value="section2">參加對象</a></li>
+	  <li role="presentation" class="btn-lg"><a href="#" value="section3">活動時程</a></li>
+	  <li role="presentation" class="btn-lg"><a href="#" value="section4">參加辦法</a></li>
+	  <li role="presentation" class="btn-lg"><a href="#" value="section5">作品說明及範例</a></li>
+	  <li role="presentation" class="btn-lg"><a href="#" value="section6">系統使用說明</a></li>
+	  <li role="presentation" class="btn-lg"><a href="#" value="section7">評選與獎勵</a></li>
+	  <li role="presentation" class="btn-lg"><a href="#" value="section8">注意事項</a></li>
+	  <li role="presentation" class="btn-lg"><a href="#" value="section9">聯絡資訊</a></li>
+	  <li role="presentation" class="btn-lg"><a href="registrationCompetition.php"><span class="red-font" id="registLink"><strong>報名比賽</strong></span></a></li>
+	</ul> -->
+	</div>
+	</div>
+	</div>
+	<!--========================header底===============================-->
+	
+	<!--========================content頂===============================-->
+	<div class="row">
+	<div class="col-lg-10 col-sm-10 col-xs-10 col-lg-offset-1 col-sm-offset-1 col-xs-offset-1"> 
+	<div id="mycontent" data-spy="scroll" data-target="#navbar-meun">
+	<!-- <div style="margin:50px auto; width:655px">
+	<img src="image/Maintenance.png" />
+	</div> -->
+	<!-- 活動宗旨 -->
+	<div id="section1">
+		<h3 class="contentTitle"><strong>活動宗旨:</strong></h3>
+		<p>
+		&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp明日創作藉由主題文章之引導，讓學生多元發想、自由寫作，並以啟發問題使學生寫作之內容架構更完整；也希望學生從明日創作的多讀多寫，能激發寫作想法、學習探索主題知識，並延伸學習興趣，進而培養終身學習習慣。
+		</p>
+		<p>
+		&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp明日創作實作包含四個步驟，分別為「讀、創、聊、登」。「讀」指的是讓學生透過異質性閱讀文章的方式，對於寫作主題有不同面向的刺激。「創」指的是學生閱讀文章後，經由啟發問題的引導，先構思關於寫作主題想要寫的內容，接著再組織這些寫作想法成為完整文章初稿。「聊」指的是學生相互閱讀同儕的初稿，並反饋建議修改意見。寫作者收到這些意見之後，可據以修改草稿。「登」指的是學生們根據同儕意見修改初稿後，將完整文章發表與全班學生分享，提供學生一個作品賞閱的舞台。
+		</p>
+		<p>
+		&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp因此，主題文章的設計，可說是明日創作的核心之一。透過設計這些主題文章，學生們從這些文章汲取關於主題的相關知識與想法，再透過啟發問題聚斂寫作內容。透過明日創作的四個步驟，讓學生不再苦於無寫作想法，不知如何下筆的困擾。
+		</p>
+		<p>
+		&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp為使桃園市師生有豐富、多元的主題文章，可供教學與學習，同時促進教師專業發展，爰於104年度規劃辦理兩期徵稿競賽，鼓勵教師、社會人士投入明日創作主題文章製作的行列，激發自創教學教材的創意與熱忱，共享教學資源。
+		</p>
+		<p><strong>主辦單位：</strong>桃園市政府教育局</p>
+		<p><strong>承辦單位：</strong>桃園市立過嶺國民中學</p>
+		<p><strong>協辦單位：</strong>國立中央大學</p>
+	</div>
+	
+	<!-- 參加對象 -->
+	<div id="section2">
+		<h3 class="contentTitle"><strong>參加對象:</strong></h3>
+		<p>
+		1.	任教於桃園市中小學教師或設籍於桃園市之社會人士
+		</p>
+		<p>
+		2.	符合上述資格者，皆可以個人名義或團體名義(4人為限)方式報名參賽
+		</p>
+		<p>
+		3.	獲獎者須於領獎時檢附身分證明如在職證明或戶籍謄本，若經查不符參賽資格，將取消得獎資格
+		</p>
+	</div>	
+	<!-- 活動時程 -->
+	<div id="section3">
+		<h3 class="contentTitle"><strong>一、活動時程:</strong></h3>
+		<p>
+			<strong>【第一期】</strong>
+		</p>
+		
+		<span style="font-size:20px">報名投稿：104年3月23日(星期一)至4月22日(星期三)</span><br>
+		<span style="font-size:20px">公告結果：104年5月20日(星期三)</span><br>
+		<span style="font-size:20px">頒獎：104年5月27日(星期三)</span><br>
+
+		<h3 class="contentTitle"><strong>二、參加辦法:</strong></h3>
+		<p>
+		&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp參賽者於投稿期間，至本競賽網站申請帳號且填具報名表後報名成功，始得於系統製作、上傳參賽作品，參加競賽。個人或團體參賽，皆不限投稿件數。	
+		</p>
+		<p><strong>一、報名：</strong></p>
+		<p>個人參賽：</p>
+		<p>(一)	以「明日學校」個人帳號登入報名系統(無帳號者請點選註冊帳號)。</p>
+		<p>(二)	點選「報名比賽」，填具報名表相關資料：姓名、性別、電子信箱、行動電話、身分別、任職單位等欄位。</p>
+		<p>(三)	送出申請後即報名成功。</p>
+		<p>團體參賽：</p>
+		<p>(一)	每一成員須先擁有「明日學校」個人帳號，無帳號者請先至系統申請。</p>
+		<p>(二)	任一成員皆可代表團隊進行報名程序(請勿重複報名)，代表報名者即為主要聯絡人：</p>
+		<ul style="font-size:20px">
+			<li>1. 以「明日學校」個人帳號登入報名系統。</li>
+			<li>2. 點選「報名比賽」，填具報名表相關資料：團隊名稱、成員(姓名、性別、電子信箱、行動電話、身分別、任職單位、明日學校個人帳號)等欄位。</li>
+			<li>3. 送出申請後即報名成功。</li>
+		</ul>
+		<p><strong>二、製作、上傳參賽作品：</strong></p>
+		<p> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp報名成功後，可點入「教材編輯」進行參賽作品製作，製作完成後，須上傳參賽作品。教材製作原則及系統使用方法請參照網站「作品說明及範例」、「系統使用說明」。</p>
+	
+	</div>
+	<!-- 參加辦法 -->
+	<div id="section4">
+		
+	</div>
+	<!-- 作品說明及範例 -->
+	<div id="section5">
+		<h3 class="contentTitle"><strong>作品說明及範例:</strong></h3>
+		<p>
+			&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp本次「明日創作主題文章」設計競賽，參賽作品內容主軸須具「知識性」，教師可配合課程單元、活動、節慶、生活經驗等發想主題文章，藉此引導學生進行主題寫作。每一主題作品須包含：
+		</p>
+		<p>1.	主題名稱：限20字以內。</p>
+		<p>2.	主題領域：對應九年一貫課程相關領域：「語文」、「社會與人文」、「自然與科學」、「健康與運動」，及相關子領域。</p>
+		<p>3.	適用對象：分為國小低、中、高年級及國中。</p>
+		<p>4.	主題文章：編輯4篇具異質性、不同角度之範文供學生閱讀，讓學生能了解主題的內容範疇，進而寫作。範文可自創或使用他人創作(請參考注意事項)，字數建議國小一篇約300-600字，國中一篇約800-1200字。</p>
+		<p>5.	啟發問題：根據主題發想相關、具層次性的問題，可引導學生進行寫作架構的思考。</p>
+		<p>6.	關鍵詞：至少設置3-5個關鍵詞，以供檢索。</p>
+
+		<p style="font-size:26px;">範例：<a href="example.pdf" target="_blank">PDF檔</a><span class="glyphicon glyphicon-search" aria-hidden="true" style="margin:0 5px;font-size:20px"></span></p>
+		
+	</div>
+	
+	
+	<!-- 系統使用說明 -->
+	<div id="section6">
+		<h3 class="contentTitle"><strong>系統使用說明:</strong></h3>
+		<h1 style="color:red">(1)報名比賽</h1>
+		<hr>
+		<img src="image/Teaching001.png"/><br>
+		<img src="image/Teaching002.png"/><br>
+		<img src="image/Teaching003.png"/><br>
+		<img src="image/Teaching004.png"/>
+		<h1 style="color:red;margin-top:100px">(2)新增主題文章</h1>
+		<hr>
+		<img src="image/Teaching005.png"/><br>
+		<img src="image/Teaching006.png"/><br>
+		<h1 style="color:red;margin-top:100px">(3)修改主題文章</h1>
+		<hr>
+		<img src="image/Teaching007.png"/><br>
+		<img src="image/Teaching008.png"/><br>
+		<h1 style="color:red;margin-top:100px">(4)刪除主題文章</h1>
+		<hr>
+		<img src="image/Teaching009.png"/><br>
+		<img src="image/Teaching010.png"/><br>
+		<h1 style="color:red;margin-top:100px">(5)編輯範例文章</h1>
+		<hr>
+		<img src="image/Teaching011.png"/><br>
+		<img src="image/Teaching012.png"/><br>
+		<img src="image/Teaching013.png"/><br>
+		<img src="image/Teaching014.png"/><br>
+		<h1 style="color:red;margin-top:100px">(6)編輯啟發問題</h1>
+		<hr>
+		<img src="image/Teaching015.png"/><br>
+		<img src="image/Teaching016.png"/><br>
+		<img src="image/Teaching017.png"/><br>
+		<h1 style="color:red;margin-top:100px">(7)由教材管理系統匯入主題</h1>
+		<hr>
+		<img src="image/Teaching023.png"/><br>
+		<img src="image/Teaching024.png"/><br>
+		<img src="image/Teaching025.png"/><br>
+		
+		<h1 style="color:red;margin-top:100px">(8)投稿主題文章</h1>
+		<hr>
+		<img src="image/Teaching018.png"/><br>
+		<img src="image/Teaching019.png"/><br>
+		<!-- <img src="image/Teaching020.png"/><br> -->
+		<h1 style="color:red;margin-top:100px">9)取消投稿主題文章</h1>
+		<hr>
+		<img src="image/Teaching021.png"/><br>
+		<img src="image/Teaching022.png"/><br>
+		
+	</div>
+	
+
+	<!-- 評選與獎勵		 -->
+	<div id="section7">
+		<h3 class="contentTitle"><strong>評選與獎勵:</strong></h3>
+		<p><strong>一、評選方式：</strong></p>
+		<ul style="font-size:20px">
+		<li><p>(一)評審委員：</p><p>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp聘請桃園市國民教育輔導團語文學習領域校長與教師、明日創作種子教師、語文教育專長之學者專家或相關領域傑出人士等進行評審。</p></li>
+		<li><p>(二)評選向度：</p><p>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp設計作品切合主題、文章涵蓋多元角度的探討、文章內容正確性、啟發性問題、切合學習年段等。</p></li>
+		</ul>
+		<p><strong>二、獎勵方式：</strong></p>
+		<ul style="font-size:20px">
+		<li><p>(一)獎勵內容：由專家評審評定，依學習年段分組遴選出每件優秀作品可獲得</p><p>1.	獎狀：乙紙</p><p>2.	教材設計費：貳仟元圖書禮卷</p><p>3.	教師著作積分：入選作品每件視同教師著作0.1分（由二人以上合作之著作，給分依作者人數平均之)。</p></li>
+		<li><p>(二)獎勵名額：各組擇優入選，總入選件數最高100件</p></li>
+		<li><p>(三)公告結果：104年5月20日(星期三)於競賽網站公布得獎作品與作者，並以電話及電子郵件通知得獎者。</p></li>
+		<li><p>(四)頒獎日期：104年5月27日(星期三)於「明日學校」高峰論壇暨表揚會會場頒獎。</p></li>
+		</ul>
+	</div>
+	
+	<!-- 注意事項 -->
+	<div id="section8">
+		<h3 class="contentTitle"><strong>注意事項:</strong></h3>
+		<p>一、	參賽者須同意參賽作品採「創用CC」(http://creativecommons.tw/explore)之相
+	    關規範使用，並放置於明日學校教材管理系統，供明日學校教師教學使用。
+		</p>
+		<p>二、	本競賽乃屬非營利教育目的之活動，於本競賽獲獎文章將作為國中小教學公開使用，  建議參賽作品之著作權合法性須符合著作權法第65條規定，並註明出處來源。</p>
+		<p><strong>著作權法第 65 條</strong></p>
+		<p>著作之合理使用，不構成著作財產權之侵害。著作之利用是否合於第四十四條至第六十三條所定之合理範圍或其他合理使用之情形，應審酌一切情狀，尤應注意下列事項，以為判斷之基準：</p>
+		<p>一、利用之目的及性質，包括係為商業目的或非營利教育目的。</p>
+		<p>二、著作之性質。</p>
+		<p>三、所利用之質量及其在整個著作所占之比例。</p>
+		<p>四、利用結果對著作潛在市場與現在價值之影響。</p>
+		<p>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp著作權人團體與利用人團體就著作之合理使用範圍達成協議者，得為前項判斷之參考。前項協議過程中，得諮詢著作權專責機關之意見。</p>
+	</div>
+	<!-- 聯絡資訊 -->
+	<div id="section9">
+		<h3 class="contentTitle"><strong>聯絡資訊:</strong></h3>
+		<p>一、競賽計畫及執行行政聯絡：過嶺國中教務主任伍孝春，03-4200026#211。</p>
+		<p>二、報名及作品上傳系統聯絡：中央大學研究助理黃瓊慧，03-4227151#35405，電子信箱：flora@cl.ncu.edu.tw。</p>
+		
+		
+	</div>
+	<!-- <p class="text-center" style="margin-top:50px">
+		<img src="image/Creating of Tomorrow-01.png"/>
+		</p>
+
+		<p class="text-center" style="margin-top:100px;margin-bottom:200px" id="section10">
+		<a href="registrationCompetition.php" data-toggle="tooltip" data-placement="left" title="Tooltip on left"><img src="image/joinBtn_link.png" id="joinBtn" /></a>
+		</p> -->
+	
+	</div>
+	</div>
+	<!-- topBtn
+	<div class="col-lg-1 col-sm-1 col-xs-1"> 
+	<button type="button" class="btn btn-default btn-lg" aria-label="Left Align" id="topBtn">
+	 <span class="glyphicon glyphicon-circle-arrow-up" aria-hidden="true" style="font-size:40px"></span>
+	</button>
+
+	</div>
+	-->
+	</div>
+
+	<?php
+	init_footer();
+
+	?>
